26593497dc8c4cb598fb5d56cd265f28f14b92f4
lixylabs <2637oben>
2016-02-21 13:03:46 +0200

    jxclsjals

--- ftapi/src/main/java/com/lixy/ftapi/controller/CommenterController.java
+++ ftapi/src/main/java/com/lixy/ftapi/controller/CommenterController.java
@@ -1,5 +1,7 @@
 package com.lixy.ftapi.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,22 +9,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lixy.ftapi.domain.FortuneRequest;
 import com.lixy.ftapi.exception.ApiException;
+import com.lixy.ftapi.model.FortuneInfo;
 import com.lixy.ftapi.model.GResponse;
 import com.lixy.ftapi.model.UserAuthentication;
 import com.lixy.ftapi.service.CommenterService;
+import com.lixy.ftapi.service.FortuneService;
 import com.lixy.ftapi.service.UtilService;
+import com.lixy.ftapi.type.StatusType;
 
 @RestController
 @RequestMapping("/v1/commenter")
 @PreAuthorize("hasRole('ROLE_ROOT') or hasRole('ROLE_COMMENTER')")
 public class CommenterController {
 	
+	private static Logger logger = LogManager.getLogger(CommenterController.class);
+	
 	@Autowired
 	private CommenterService commenterService;
 	
 	@Autowired
 	private UtilService utilService;
+	
+	@Autowired
+	private FortuneService fortuneService;
 
 	@RequestMapping(value = "/get_fortune_list/{status}/{start}/{limit}", method = RequestMethod.GET)
 	public GResponse getFortuneRequestByStatus(
@@ -47,6 +58,30 @@ public class CommenterController {
 			response.convertToGResponse(e);
 		}
 
+		return response;
+	}
+	
+	@RequestMapping(value = "/get_fortune_request/{id}", method = RequestMethod.GET)
+	public GResponse getFortuneRequest(UserAuthentication auth, @PathVariable(value = "id") Long requestId) {
+		GResponse response = new GResponse();
+		response.setUid(auth.getUser().getId());
+
+		logger.info(auth.getUser().getUsername() + " asked for fortune request info for " + requestId);
+
+		try {
+			FortuneInfo info = new FortuneInfo();
+
+			FortuneRequest request = fortuneService.getFortuneRequestById(requestId);
+			fortuneService.convertToRequestModel(request, info);
+
+			response.setStatus(StatusType.OK);
+			response.setObject(info);
+
+		} catch (Exception ex) {
+			response.convertToGResponse(ex);
+		}
+
+		logger.info(response);
 		return response;
 	}
 
